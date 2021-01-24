06b098261e1132a97e598ea2ae6fe64ef43be774
Dmitry <dmitry_dudenin@mail.ru>
2021-01-24 17:09:56 +0300

    Add login form

---
+++ Swift_CustomApp/LoginFormController.swift
@@ -0,0 +1,85 @@
+//
+//  LoginFormController.swift
+//  Swift_CustomApp
+//
+//  Created by Дмитрий on 23.01.2021.
+//
+
+import UIKit
+
+class LoginFormController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet var loginInput: UITextField!
+    @IBOutlet var passwordInput: UITextField!
+    @IBOutlet var scrollView: UIScrollView!
+    
+    let userData = (login: "Tim Cook", password: "Apple1e12$")
+    
+    // Когда клавиатура появляется
+    @objc func keyboardWasShown(notification: Notification) {
+        // Получаем размер клавиатуры
+        let info = notification.userInfo! as NSDictionary
+        let kbSize = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
+        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: kbSize.height, right: 0.0)
+        
+        // Добавляем отступ внизу UIScrollView, равный размеру клавиатуры
+        self.scrollView?.contentInset = contentInsets
+        scrollView?.scrollIndicatorInsets = contentInsets
+    }
+    
+    //Когда клавиатура исчезает
+    @objc func keyboardWillBeHidden(notification: Notification) {
+        // Устанавливаем отступ внизу UIScrollView, равный 0
+        let contentInsets = UIEdgeInsets.zero
+        scrollView?.contentInset = contentInsets
+    }
+    
+    private func switchBasedNextTextField(_ textField: UITextField) {
+        switch textField {
+        case loginInput:
+            passwordInput.becomeFirstResponder()
+        default:
+            passwordInput.resignFirstResponder()
+        }
+    }
+    
+    // Called on returnKey pressed
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        switchBasedNextTextField(textField)
+        return false
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Подписываемся на два уведомления: одно приходит при появлении клавиатуры
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWasShown), name: UIResponder.keyboardWillShowNotification, object: nil)
+        // Второе — когда она пропадает
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillBeHidden(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @IBAction func loginButtonPressed(_ sender: Any) {
+        // Получаем текст логина
+        let login = loginInput.text!
+        // Получаем текст-пароль
+        let password = passwordInput.text!
+        
+        // Проверяем, верны ли они
+        if login == userData.login && password == userData.password {
+            print("Добрый день, \(login)")
+        } else {
+            print("Ошибка авторизации")
+        }
+    }
+}
